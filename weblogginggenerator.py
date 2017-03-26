37fc403da6328be92df1cb2664bd0650b94e4323
CodingCat <zhunansjtu@gmail.com>
2017-03-25 19:36:29 -0700

    test

--- weblogginggenerator.py
+++ weblogginggenerator.py
@@ -21,11 +21,9 @@ def usage():
 def server_loop():
     global target
 
-    if not len(target):
-        target = '0.0.0.0'
-
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target, port))
+    print('listen on %s:%s' % (target, port))
     server.listen(10)
 
     while True:
@@ -35,22 +33,6 @@ def server_loop():
             args=(client_socket,)
         )
         client_thread.start()
-
-
-def run_command(cmd):
-    command = cmd.rstrip()
-
-    try:
-        output = subprocess.check_output(
-            command,
-            stderr=subprocess.STDOUT,
-            shell=True
-        )
-    except:
-        output = 'Failed to execute command\r\n'
-
-    return output
-
 
 def client_handler(client_socket):
     try:
